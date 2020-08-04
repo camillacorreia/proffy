0c56fa0dfbdf79c8aedffdbdc039fff12efbbcc1
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-04 18:18:41 -0300

    feat: criação do componente pageheader

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
-function TeacherList() {
+import './styles.css';
+
+function TeacherList(){
     return (
-        <h1>Teachers</h1>
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis." />
+        </div>
     );
 };
 
