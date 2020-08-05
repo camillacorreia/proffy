8e170a2031db9db6aed72539486836e8339e2340
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-04 23:48:18 -0300

    feat: configuração da rota
    Utilização do insomnia

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    
+    const users = [
+        {name: 'Camilla', age: 25 }
+    ];
+
+    return response.json(users);
+});
+
+app.listen(3333);
