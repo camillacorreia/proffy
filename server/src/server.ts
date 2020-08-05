7768d2b94a11524b5ea888b4d069f277f4c27fc8
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-05 16:24:51 -0300

    feat: criação da rota para criar aulas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/users', (request, response) => {
-    console.log(request.body);
-    
-    const users = [
-        {name: 'Camilla', age: 25 }
-    ];
-
-    return response.json(users);
-});
+app.use(routes);
 
 app.listen(3333);
