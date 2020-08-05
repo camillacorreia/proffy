5478d566475fd68bfb5d391d5632a807e63e1e46
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-04 21:49:39 -0300

    feat: criação da listagem dos professores

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import wppIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeacherList(){
@@ -24,6 +26,33 @@ function TeacherList(){
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars3.githubusercontent.com/u/50275354?s=460&u=ab6a13576a9ed46db6faa2376c5ae2311bc492d3&v=4" alt="Camilla Correia"/>
+                        <div>
+                            <strong>Camilla Correia</strong>
+                            <span>Programação</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Entusista das melhores tecnologias.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={wppIcon} alt="whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     );
 };
