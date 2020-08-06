738c5c0562f164022fbff869114d2384113c7ab3
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-06 19:19:12 -0300

    feat: criação da div horário das aulas
    Corrigido o style do TeacherItem também

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -43,6 +43,36 @@ function TeacherForm() {
 
                 </fieldset>
 
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                        <button
+                            type="button">
+                            + Novo horário
+                        </button>
+                    </legend>
+
+                    <div className="schedule-item">
+                        <Select
+                            name="week_day"
+                            label="Dia da Semana"
+                            options={[
+                                { value: '0', label: 'Domingo' },
+                                { value: '1', label: 'Segunda' },
+                                { value: '2', label: 'Terça' },
+                                { value: '3', label: 'Quarta' },
+                                { value: '4', label: 'Quinta' },
+                                { value: '5', label: 'Sexta' },
+                                { value: '6', label: 'Sábado' },
+                            ]}
+                        />
+
+                        <Input name="from" label="Das" type="time" />
+                        <Input name="to" label="Até" type="time" />
+                    </div>
+
+                </fieldset>
+
                 <footer>
                     <p>
                         <img src={warningIcon} alt="Aviso Importante" />
