640ef5ca61ec41993e93036a7548b284b9b2c492
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-06 19:36:16 -0300

    fix: corrigindo select

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,8 +15,8 @@ interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     return (
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-            <select id={name} {...rest} >
-                <option value="" disabled selected hidden>Selecione uma opção</option>
+            <select defaultValue="" id={name} {...rest} >
+                <option value="" disabled hidden>Selecione uma opção</option>
                 {options.map(option => {
                     return <option key={option.value} value={option.value}>{option.label}</option>
                 })}
