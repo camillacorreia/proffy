837e2510010357d08ac9a8d8a7e6cd02d5f47ca1
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-08-06 18:15:46 -0300

    feat: criação do componente input

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import PageHeader from "../../components/PageHeader/";
+import Input from '../../components/Input';
+
+import warningIcon from "../../assets/images/icons/warning.svg";
 
 import './styles.css';
 
@@ -16,22 +19,28 @@ function TeacherForm() {
                 <fieldset>
                     <legend>Seus dados</legend>
 
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type="text" id="name" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="name">Avatar</label>
-                        <input type="text" id="avatar" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="name">WhatsApp</label>
-                        <input type="number" id="whatsapp" />
-                    </div>
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" type="number" />
 
                 </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" type="number" />
+
+                </fieldset>
+                
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso Importante" />
+                        Importante <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">Salvar cadastro</button>
+                </footer>
             </main>
         </div>
     )
